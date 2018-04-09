67562441de4803cdb9f35183275e0e55528ab0be
Scott Roberts <s.e.roberts0@gmail.com>
2018-04-09 09:06:48 -0700

    Lab4 Init + Some Work

---
+++ Lab4_RailwayToHell/RailwayToHell/MonteCarlo/Program.fs
@@ -0,0 +1,34 @@
+﻿type Point = {xCoord: float; yCoord:float}
+
+let point1 = 
+    {xCoord = 0.1; yCoord = 0.1}
+
+let point2 =
+    {xCoord = 0.8; yCoord = 0.8}
+
+let makePoint x y =
+    {xCoord = x; yCoord = y}
+
+let r = System.Random()
+
+let randomPoint () =
+    let x = r.NextDouble()
+    let y = r.NextDouble()
+    makePoint x y
+
+let throwDarts n =
+    List.init n (fun n -> randomPoint())
+
+type HitResult = 
+    | Hit
+    | Miss of float
+
+let isHit point =
+    sqrt (pown point.xCoord 2 + pown point.yCoord 2) <= 1.0
+
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
